efc1f4bc30889c3fedb71ae16c19e8126273ae08
el-damiano <el9damiano@gmail.com>
2024-10-02 20:45:55 +0200

    perf: limit to 60fps

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ Screen height: {SCREEN_HEIGHT}""")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0
 
     while True:
         screen.fill("black")
@@ -17,6 +19,8 @@ Screen height: {SCREEN_HEIGHT}""")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        delta_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
