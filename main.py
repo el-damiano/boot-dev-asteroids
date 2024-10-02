08235e86c8220e04a19addd3dde0e1cc6b0bc6d2
el-damiano <el9damiano@gmail.com>
2024-10-02 20:28:20 +0200

    feat: draw black screen

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+def main():
+    print(f"""Starting asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}""")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
