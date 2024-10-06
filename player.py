19c34468d9f58f9bb453e6840979bb97f00980ad
el-damiano <el9damiano@gmail.com>
2024-10-06 15:23:07 +0200

    refactor: add a rate limit for player bullets

--- player.py
+++ player.py
@@ -1,7 +1,13 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN
+)
 
 
 class Player(CircleShape):
@@ -9,6 +15,7 @@ class Player(CircleShape):
     def __init__(self, x, y) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +30,7 @@ class Player(CircleShape):
 
     def update(self, delta_time):
         keys = pygame.key.get_pressed()
+        self.timer -= delta_time
 
         if keys[pygame.K_w]:
             self.move(delta_time)
@@ -43,6 +51,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * delta_time
 
     def shoot(self):
+        if self.timer > 0:
+            return
+
+        self.timer = PLAYER_SHOOT_COOLDOWN
         x, y = self.position
         shot = Shot(x, y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
